98398797fb533b79c43583950cb8280dad27e687
Joey Ssekono <ssekono@gmail.com>
2013-10-26 12:06:14 +0300

    First Stable release of v1.0

--- js/waterbird.form.config.js
+++ js/waterbird.form.config.js
@@ -1,14 +1,45 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-//Each field on the form
+	$('#text').focusout(function() {
+		$(this).waterBird({
+			type: 'text',
+			required: 'yes',
+		});
+	});
 
-  $("#fname").focusout(function() {
-      validateText("#fname", ".fname-warning");
-      validateRequired("#fname", ".fname-warning");
-  });
+	$('#number').focusout(function() {
+		$(this).waterBird({
+			type: 'number',
+			required: 'yes',
+		});
+	});
+	
+	$('#email').focusout(function() {
+		$(this).waterBird({
+			type: 'email',
+			required: 'yes',
+		});
+	});
+	
+	$('#url').focusout(function() {
+		$(this).waterBird({
+			type: 'url',
+			required: 'yes',
+		});
+	});
+	
+	$('#phone').focusout(function() {
+		$(this).waterBird({
+			type: 'phone',
+			required: 'yes',
+		});
+	});
+	
+	$('#date').focusout(function() {
+		$(this).waterBird({
+			type: 'date',
+			required: 'yes',
+		});
+	});
 
-  $(".required").focusin(function(){
-
-  });
- 
- });
+}); 
--- js/waterbird.form.validator.js
+++ js/waterbird.form.validator.js
@@ -1,214 +1,325 @@
-$(document).ready(function () {
+/*! Copyright (c) 2013 Joe Ssekono (http://josephssekono.novariss.com)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Version: 1.0.0
+ *
+ * Requires: 1.8.0+
+ */
 
-//  $(".required").focusin(function(){ });
+(function($) {
+
+	$.fn.extend({
+
+		// Plugin Name
+		waterBird : function(options) {
+
+			// Defaults options are set here
+			var defaults = {
+					type : 'text',
+					required : 'yes',
+				};
+			
+			var options = $.extend(defaults, options);
+
+			return this.each(function() {
+				
+				var element = '#' + this.id;
+				var message = element + '-msg';
+				
+				//Setting up variable to check whether its required
+				var status = true;
+				if (options.required == 'yes'){
+					status = true;
+				}
+				else{
+					status = false;
+				}
+				
+
+				switch (options.type) {
+					case 'text':
+						if (options.required == 'yes') {
+							// check for empty field
+							if (validateRequired(element, message)) {
+								// validate input
+								validateText(element, message, status);
+								}
+							} 
+						else {
+							// validate input
+							validateText(element, message, status);
+							}
+						break;
+
+					case 'number':
+						if (options.required == 'yes') {
+							// check for empty field
+							if (validateRequired(element, message)) {
+								// validate input
+								validateNumber(element, message, status);
+								}
+							} 
+						else {
+							// validate input
+							validateNumber(element, message, status);
+							}
+						break;
+
+					case 'phone':
+						if (options.required == 'yes') {
+							// check for empty field
+							if (validateRequired(element, message)) {
+								// validate input
+								validatePhone(element, message, status);
+								}
+							} 
+						else {
+							// validate input
+							validatePhone(element, message, status);
+							}
+						break;
+
+					case 'email':
+						if (options.required == 'yes') {
+							// check for empty field
+							if (validateRequired(element, message)) {
+								// validate input
+								validateEmail(element, message, status);
+								}
+							} 
+						else {
+							// validate input
+							validateEmail(element, message, status);
+							}
+						break;
+
+					case 'url':
+						if (options.required == 'yes') {
+							// check for empty field
+							if (validateRequired(element, message)) {
+								// validate input
+								validateUrl(element, message, status);
+								}
+							} 
+						else {
+							// validate input
+							validateUrl(element, message, status);
+							}
+						break;
+
+					case 'date':
+						if (options.required == 'yes') {
+							// check for empty field
+							if (validateRequired(element, message)) {
+								// validate input
+								validateDate(element, message, status);
+								}
+							} 
+						else {
+							// validate input
+							validateDate(element, message, status);
+							}
+						break;
+
+						default:
+
+					}
+				
+				
+
+				
+				
+				
+				/**
+				 * a is input field id name 
+				 * b is invalid input message <p> class name
+				 * 
+				 * Function validates filled fields only
+				 */
+				
+				function validateRequired(a, b, c) {
+					//Checking if field is empty
+					if ($(a).val() == "") {
+						$(b).text("Field is required. Cannot be left empty")
+							.attr("class", "invalid");
+						return false;
+					} 
+					else {
+						$(b).text("valid").attr("class", "valid");
+						return true;
+					}
+				}
+				
+
+				/**
+				 *  a is input field id name 
+				 *  b is invalid input message <p> class name
+				 * c is boolean value for required
+				 *  
+				 *  Function validates only text content 
+				 */
+				
+
+				function validateText(a, b, c) {
+					// Value in the field
+					var text = $(a).val();
+					// Regular Expression to validate value against
+					var regularExpression = /^[a-zA-Z]+$/;
+
+					if (c) {
+						// Validating field content
+						if (!regularExpression.test(text)) {
+							$(b).text("Your input is invalid. Text only!")
+									.attr("class", "invalid");
+						} else {
+							$(b).text("valid").attr("class", "valid");
+						}
+					}
+
+				}
 
 
-  //Each field on the form
+				/**
+				 *  a is input field id name 
+				 *  b is invalid input message <p> class name
+				 *  c is boolean value for required
+				 *  
+				 *  Function validates only number content 
+				 */
+								
+				function validateNumber(a, b, c) {
+					// Value in the field
+					var number = $(a).val();
+					// Regular Expression to validate value against
+					var regularExpression = /^[0-9]+$/;
 
-  $("#fname").focusout(function() {
-      validateText("#fname", ".fname-warning");
-      validateRequired("#fname", ".fname-warning");
-  });
-
-  $("#lname").focusout(function() {
-      validateText("#lname", ".lname-warning");
-      validateRequired("#lname", ".lname-warning");
-  });
-
-  $("#age").focusout(function() {
-      validateNumber("#age", ".age-warning");
-      validateRequired("#age", ".age-warning");  
-  });
-
-  $("#email").focusout(function() {
-      validateEmail("#email", ".email-warning");
-      validateRequired("#email", ".email-warning");
-  });
-
-  $("#website").focusout(function() {
-      validateWebsite("#website", ".website-warning");
-      validateRequired("#website", ".website-warning");
-  });
-
-  $("#phone").focusout(function() {
-      validatePhone("#phone", ".phone-warning");
-      validateRequired("#phone", ".phone-warning");
-  });
-
-  $("#password").focusout(function() {
-      //validatePassword("#password", ".password-warning");
-      validateRequired("#password", ".password-warning");
-  });
-
-  $("#password-2").focusout(function() {
-      validateCompare("#password", "#password-2","Passwords",  ".password-2-warning");
-      validateRequired("#password-2", ".password-2-warning");
-  });
+					if (c) {
+						// Validating field content
+						if (!regularExpression.test(number)) {
+							$(b).text("Your input is invalid. Numbers only!")
+									.attr("class", "invalid");
+						} else {
+							$(b).text("valid").attr("class", "valid");
+						}
+					}
+				}
 
 
-  
-  
-  
-  
-  
-  
-  
-  
-//  ===================================================================
-//  Function definitions
-  
-  
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validateText(a,b) {
-    var text =$(a).val();
- //   if (!(text == NULL)) {
-        var regularExpression = /^[a-zA-Z]+$/; 
-        if(!regularExpression.test(text) ) { 
-          $(b).text("Your input is invalid. Text only!").attr( "class", b.replace(".", "")+" invalid" );  
-        }
-        else {
-          $(b).text("").attr( "class", b.replace(".", "")+" valid");
-        } 
- //   }
-  }  
-  
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validatePhone() {
-    var text =$(a).val();
-    
-    
-   // var regularExpression = /^(+)+[0-9]+$/;     
-    var regularExpression = /^[0-9]+$/;     
-    if(!regularExpression.test(text) ) { 
-      $(b).text("Not a valid Phone number!").attr( "class", b.replace(".", "")+" invalid" );  
-    }
-    else {
-      $(b).text("").attr( "class", b.replace(".", "")+" valid");
-    } 
-  }  
-  
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validateNumber() {
-    var text =$(a).val();
-    //var regularExpression = /^d+$/; 
-    var regularExpression = /^[0-9]+$/;     
-    if(!regularExpression.test(text) ) { 
-      $(b).text("Your input is invalid. Numbers only!").attr( "class", b.replace(".", "")+" invalid" );  
-    }
-    else {
-      $(b).text("").attr( "class", b.replace(".", "")+" valid");
-    } 
-  }
-  
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validateEmail(a,b) {
-      var emailAddress = $(a).val();
-      var regularExpression = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-      
-      //testing if all email requirements are there
-          if(!regularExpression.test(emailAddress) ) {
-          //testing if all validated requirements are positioned correctly
-              var atpos = emailAddress.indexOf("@");
-              var dotpos = emailAddress.lastIndexOf(".");
-              if (atpos < 1 || dotpos < atpos+2 || dotpos+2 >= x.length) {
-                $(b).text("Not a valid e-mail address").attr( "class", b.replace(".", "")+" invalid" );
-              }
-              else {
-                $(b).text("").attr( "class", b.replace(".", "")+" valid" );
-              }
-          }
-          else {
-            $(b).text("Not a valid e-mail address").attr( "class", b.replace(".", "")+" invalid" );
-          }
-  }
+				/**
+				 *  a is input field id name 
+				 *  b is invalid input message <p> class name
+				 *  c is boolean value for required
+				 *  
+				 *  Function validates only email
+				 */
 
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validateWebsite() {
-      var url = $(a).val();  
-      var regularExpression = /^(https?|ftp|file):\/\/.+$/;
-  
-      //testing if all url requirements are there
-          if(!regularExpression.test(url) ) {
-            $(b).text("Not a valid website URL").attr( "class", b.replace(".", "")+" invalid" );
-          }
-          else {
-            $(b).text("").attr( "class", b.replace(".", "")+" valid" );
-          }
-  }
+				function validateEmail(a, b, c) {
+					// Value in the field
+					var email = $(a).val();
+					// Regular Expression to validate value against
+					var regularExpression = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+					if (c) {
+						// Validating field content
+						if (!regularExpression.test(email)) {
+							$(b).text("Your input is invalid. Email only!")
+									.attr("class", "invalid");
+						} else {
+							$(b).text("valid").attr("class", "valid");
+						}
+					}
+				}
+				
+				
+				
+				/**
+				 *  a is input field id name 
+				 *  b is invalid input message <p> class name
+				 *  c is boolean value for required
+				 *  
+				 *  Function validates only url
+				 */
+
+				function validateUrl(a, b, c) {
+					// Value in the field
+					var url = $(a).val();
+					// Regular Expression to validate value against
+					var regularExpression = /^((https?|ftp):\/\/|(www|ftp)\.)[a-z0-9-]+(\.[a-z0-9-]+)+([\/?].*)?$/;
+
+					if (c) {
+						// Validating field content
+						if (!regularExpression.test(url)) {
+							$(b).text("Your input is invalid. Url only!")
+									.attr("class", "invalid");
+						} else {
+							$(b).text("valid").attr("class", "valid");
+						}
+					}
+				}
+
+
+
+				/**
+				 *  a is input field id name 
+				 *  b is invalid input message <p> class name
+				 *  c is boolean value for required
+				 *  
+				 *  Function validates only phone numbers using international EPP format
+				 */
+
+				function validatePhone(a, b, c) {
+					// Value in the field
+					var phone = $(a).val();
+					// Regular Expression to validate value against
+					var regularExpression = /^\+[0-9]{1,3}\-[0-9]{4,14}(?:x.+)?$/;
+
+					if (c) {
+						// Validating field content
+						if (!regularExpression.test(phone)) {
+							$(b).text("Your input is invalid. Phone number only!")
+									.attr("class", "invalid");
+						} else {
+							$(b).text("valid").attr("class", "valid");
+						}
+					}
+				}
 
 
 
 
+				/**
+				 *  a is input field id name 
+				 *  b is invalid input message <p> class name
+				 *  c is boolean value for required
+				 *  
+				 *  Function validates only date using d/m/yy or dd/mm/yyyy format
+				 */
+
+				function validateDate(a, b, c) {
+					// Value in the field
+					var date = $(a).val();
+					// Regular Expression to validate value against
+					var regularExpression = /^(3[01]|[12][0-9]|0?[1-9])-(1[0-2]|0?[1-9])-(?:[0-9]{2})?[0-9]{2}$/;
+
+					if (c) {
+						// Validating field content
+						if (!regularExpression.test(date)) {
+							$(b).text("Your input is invalid. Date number only!")
+									.attr("class", "invalid");
+						} else {
+							$(b).text("valid").attr("class", "valid");
+						}
+					}
+				}
 
 
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validateRequired(a,b) {
-  //  if ($(a).is(":empty")) {
-    if ($(a).val() == NULL) {
-      $(b).text("Field is required. Cannot be left empty").attr( "class", b.replace(".", "")+" invalid" );
-    } 
-    else {
-      $(b).text(" ").attr( "class", b.replace(".", "")+" valid" );
-    }
-  }
 
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validateStrongPassword() {
-    if ($(".required").is(":empty")) {
-      $(".warning").text("empty input");
-    } 
-  }
-
-/*
-* a is input field id name
-* b is invalid input message p class name
-*/
-  function validatePassword() {
-    if ($(".required").is(":empty")) {
-      $(".warning").text("empty input");
-    } 
-  }
-
-/*
-* a is  first input field id name
-* b is  second input field id name
-* c is  name of input fields being compared
-* d is name of id to hold message output
-*/
-  function validateCompare(a,b,c,d) {
-    var item_1 = $(a).val(); //first item
-    var item_2 = $(b).val(); //second item to compare with first item
-    var msg = $(c);  //name of input fields being compared
-    var msg = $(d);  //name of id to hold message output
-  
-    if (!(item_1 == item_2)) {
-      $(d).text(msg+" do not match!").attr( "class", d.replace(".", "")+" invalid");
-    } 
-    else {
-      $(d).text(msg+" do match!").attr( "class", d.replace(".", "")+" valid");
-    } 
-    
-  }
-
-});
+				
+				
+				
+				
+	
+			});
+		}
+	});
+})(jQuery);
